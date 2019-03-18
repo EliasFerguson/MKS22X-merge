383e438da5c7e31b991c04b994519d619e44b635
Elias Ferguson <eferguson00@stuy.edu>
2019-03-18 11:28:05 -0400

    starting mergesort, working on splitting the array into two halves

--- Merge.java
+++ Merge.java
@@ -4,6 +4,10 @@ public class Merge {
     mergesort(data, 0, data.length - 1);
   }
   public static void mergesort(int[] data, int lo, int hi) {
-    
+    if (lo >= hi) return;
+    int half = data.length / 2;
+    int[] left = new int[half];
+    int[] right = new int[data.length - half];
+
   }
 }
