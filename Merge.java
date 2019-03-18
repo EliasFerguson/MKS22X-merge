70e12fd47d6aca0522fae10a3e4a33f9ddb23876
Elias Ferguson <eferguson00@stuy.edu>
2019-03-18 11:09:32 -0400

    added skeleton

---
+++ Merge.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Merge {
+  public static void mergesort(int[] data) {
+    mergesort(data, 0, data.length - 1);
+  }
+  public static void mergesort(int[] data, int lo, int hi) {
+    
+  }
+}
