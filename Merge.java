54a1f4b76609320eb6d3089859b047d448ee3c43
Elias Ferguson <eferguson00@stuy.edu>
2019-03-19 11:41:46 -0400

    initRight and initLeft work for everything

--- Merge.java
+++ Merge.java
@@ -2,15 +2,15 @@ import java.util.*;
 import java.io.*;
 public class Merge {
   public static void main(String[] args) {
-    int[] test = {1, 2, 5, 2, 7, 9 , 2, 6, 9, 10};
+    int[] test = {1, 2, 5, 2, 7, 9 , 2, 6, 9, 10, 11};
     //mergesort(test);
     int half = test.length / 2;
     int[] left = new int[half];
     int[] right = new int[test.length - half];
     left = initLeft(test, left);
     right = initRight(test, right);
-    //for (int i:left) System.out.print(i + " ");
-    for (int i:right) System.out.print(i + " ");
+    for (int i:left) System.out.print(i + " ");
+    //for (int i:right) System.out.print(i + " ");
   }
   public static void mergesort(int[] data) {
     mergesort(data, 0, data.length - 1);
